f3f902938ed46158f0b24d60707fb6417d788401
car <carmenn.choww@gmail.com>
2017-12-09 15:47:22 -0800

    vue setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var vueify = require('vueify');
 
 gulp.task('sass', function() {
   return gulp.src('assets/scss/*.scss')
@@ -7,6 +10,16 @@ gulp.task('sass', function() {
              .pipe(gulp.dest('assets/css'))
 });
 
+gulp.task('vueify', function() {
+  return browserify('./assets/vue/tags.js')
+         .transform(vueify)
+         .bundle()
+         .pipe(source('tags.js'))
+         .pipe(gulp.dest('./assets/js'))
+});
+
 gulp.task('watch', function() {
   gulp.watch('assets/scss/**/*.scss', ['sass']);
+  gulp.watch('assets/vue/**/*.js', ['vueify']);
 });
+
