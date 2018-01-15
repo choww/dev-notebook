155b6f1661b13972d20fbc2fdd3e8eee6479af2c
car <carmenn.choww@gmail.com>
2018-01-14 16:37:31 -0800

    edit tags

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -2,8 +2,9 @@
 var Vue = require('vue/dist/vue');
 
 Vue.component('tag-form', {
+  props: ['currTags'],
   data: function() {
-    return { tags: '' }
+    return { tags: this.currTags || '' }
   },
   computed: {
     processedTags: function() {
@@ -18,17 +19,15 @@ Vue.component('tag-form', {
     }
   },
   template: `
-    <div>
-      <div class="field">
-        <label for="id_categories">Tags</label>
-        <div class="tags">
-          <span :class="{'tag is-info': tag != ''}" v-for="tag in this.processedTags">
-            {{ tag }}
-          </span>
-        </div>
-        <div class="control">
-          <input id="id_categories" name="categories" type="text" class="input" v-model="tags"></input>
-        </div>
+    <div class="field">
+      <label for="id_categories">Tags</label>
+      <div class="tags">
+        <span :class="{'tag is-info': tag != ''}" v-for="tag in this.processedTags">
+          {{ tag }}
+        </span>
+      </div>
+      <div class="control">
+        <input id="id_categories" name="categories" type="text" class="input" v-model="tags"></input>
       </div>
     </div>`
 });
