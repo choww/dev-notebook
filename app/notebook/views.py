1071a35960f58d0456ad1059d6466626fb7c1a94
car <carmenn.choww@gmail.com>
2018-03-29 15:29:32 -0700

    fix error when tags in URLs had a space

--- app/notebook/views.py
+++ app/notebook/views.py
@@ -12,14 +12,15 @@ def create(request):
             return redirect('/'+request.user.username)
     else: 
         form = CreatePostForm()
-    return render(request, 'notebook/new.html', {'form': form, 'tags': tags(request.user)}) 
+    return render(request, 'notebook/new.html', 
+                  {'form': form, 'tags': tags(request.user), 'user': request.user}) 
 
 def index(request, username):
     user = User.objects.get(username=username)
     posts = user.post_set.all().order_by('-date')
     return render(request, 
                   'notebook/index.html', 
-                  {'posts': posts, 'tags': tags(user)})
+                  {'posts': posts, 'tags': tags(user), 'user': user})
 
 @login_required
 def edit(request, id):
@@ -32,13 +33,18 @@ def edit(request, id):
         if form.is_valid():
             form.save(post)
             return redirect('/'+request.user.username)
-    return render(request, 'notebook/edit.html', {'form': form, 'pk': id, 'tags': tags(request.user)})
+    return render(request, 
+                  'notebook/edit.html', 
+                  {'form': form, 'pk': id, 'user': request.user})
 
 @login_required
-def tag(request, name):
+def tag(request, username, name):
+    user = get_object_or_404(User, username=username)
     category = get_object_or_404(Category, name=name)
     tagged_posts = request.user.tag_set.filter(category_id=category.id).order_by('-post_id')
-    return render(request, 'tag/show.html', {'tagged_posts': tagged_posts, 'category': category, 'tags':  tags(request.user)})
+    return render(request, 
+                  'tag/show.html', 
+                  {'tagged_posts': tagged_posts, 'category': category, 'tags':  tags(user), 'user': user})
 
 @login_required
 def destroy(request, id):
